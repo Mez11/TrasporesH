d2c796a948cd746cd89cfc54ef9d01a15d94c6d2
Misael Alanis <misaelarturo@gmail.com>
2016-06-22 19:11:56 -0500

    Ultimas modificaciones para terminar la funcionalidad

--- src/co/persistencia/test/Principal.java
+++ src/co/persistencia/test/Principal.java
@@ -30,12 +30,13 @@ public class Principal {
 		session.beginTransaction();
 		session.save(camion);
 		session.getTransaction().commit();
-		//Obtener 
-		//Camion camion2 =(Camion) session.get(Camion.class,1);//Trae el atributo 5 del query
 		
-		System.out.println("ID"+ camion.getId());
-		System.out.println("MATRICULA"+ camion.getMatricula());
-		System.out.println("POTENCIA"+ camion.getPotencia());
+		//Obtener el camion con ID 1
+		Camion camion2 =(Camion) session.get( Camion.class, 1 );
+		
+		System.out.println("ID: "+ camion2.getId());
+		System.out.println("MATRICULA: " + camion2.getMatricula());
+		System.out.println("POTENCIA: " + camion2.getPotencia());
 		//Para las Llaves anidadas
 		//List <Camion> camiones =(List<Camion>)session.createQuery("FROM camion");
 		
@@ -46,8 +47,8 @@ public class Principal {
 		int i =0;
 		for (Camion camion3 :camiones){
 			i++;
-			System.out.println(i +".ID"+camion3.getId());
-			System.out.println(i+".MATRICULA"+camion3.getMatricula());
+			System.out.println(i +".ID: "+camion3.getId());
+			System.out.println(i+".MATRICULA: "+camion3.getMatricula());
 		}
 		
 		//Actualizar con HQL
@@ -57,7 +58,7 @@ public class Principal {
 		Query query = session.createQuery(hql);
 		//cLAVE ,VALOR .PARAMETRO,VALOR
 		query.setParameter("matricula", "TYUEP");
-		query.setParameter("id", 5);
+		query.setParameter("id", 6);
 		query.executeUpdate();
 		session.getTransaction().commit();
 		
